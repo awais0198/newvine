1616dfa5d709aca4d28cadde550ab0b79f3fb652
dev-amirson <dev.amirson@gmail.com>
2024-02-07 07:52:56 +0500

    Initial Commit

---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+export const TEMP_NAMES = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+]
+
+export const COLORS = {
+  primaryBlue: '#041D59',
+  successGreen: '#6DAD80',
+  secondaryAmber: '#FF9A1E',
+  lightGray: '#DDDDDD',
+  textGray: '#697177'
+}
